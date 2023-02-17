e62a31bb579a5488da336793f903fd7853e48bc9
PGB <pabliskidx9@gmail.com>
2023-02-16 23:02:35 -0600

    * Event click listener implemented on all buttons. * Theme changed. * Implementation of user and password validation.

--- app/src/main/java/com/third/loginscreenandroid/MainActivity.kt
+++ app/src/main/java/com/third/loginscreenandroid/MainActivity.kt
@@ -3,33 +3,54 @@ package com.third.loginscreenandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
+import com.third.loginscreenandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var background: ImageView
-    private lateinit var userimage: ImageView
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        background = findViewById(R.id.background)
-        userimage = findViewById(R.id.userImage)
+        binding.loginButton.setOnClickListener { view ->
+            val user = binding.userTextField.editText?.text.toString()
+            val password = binding.passTextField.editText?.text.toString()
+            if (user.isBlank() || password.length < 8) {
+                Toast.makeText(this, R.string.blankUser, Toast.LENGTH_LONG).show()
+            } else {
+                val userSnackBar = Snackbar
+                    .make(binding.root, getString(R.string.correctUserMessage, user), Snackbar.LENGTH_LONG)
+                    .setBackgroundTint(getColor(R.color.primario))
+                    .show()
+            }
+        }
 
-        loadBackground()
+        binding.forgotPasswordButton.setOnClickListener{ view ->
+            Toast.makeText(this, R.string.restorePassword, Toast.LENGTH_LONG).show()
+        }
+
+        binding.createAccountButton.setOnClickListener{ view ->
+            Toast.makeText(this, R.string.accountMessage, Toast.LENGTH_LONG).show()
+        }
+
+
+        loadImages()
 
     }
 
-    private fun loadBackground() {
+    private fun loadImages() {
         Glide.with(this)
         .load("https://images.unsplash.com/photo-1506914426289-e7fec369f90a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80")
         .centerCrop()
-        .into(background)
+        .into(binding.background)
 
         Glide.with(this)
         .load("https://www.pngkit.com/png/full/281-2812821_user-account-management-logo-user-icon-png.png")
         .centerCrop()
-        .into(userimage)
+        .into(binding.userImage)
     }
-
 
 }
